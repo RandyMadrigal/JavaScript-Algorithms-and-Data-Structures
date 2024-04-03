2eb7e8b57458cccd1caa4b9b52519586f464f867
Randy Madrigal <randymadrigalx12@gmail.com>
2024-04-02 22:06:56 -0400

    dev branch to do test

--- Palindrome_checker/src/script.js
+++ Palindrome_checker/src/script.js
@@ -1,1 +1,27 @@
-alert("Hola mundo :)");
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const form = document.getElementById("myForm");
+const result = document.getElementById("result");
+let palindrome = [];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  switch (textInput.value.trim().length) {
+    case 0:
+      alert("Please input a value");
+      break;
+    case 1:
+      result.innerText = `${textInput.value} is a palindrome`;
+      break;
+    default:
+      isPalindrome(textInput.value);
+  }
+});
+
+function validate() {}
+
+function isPalindrome(word) {
+  palindrome = [...word.toLowerCase()];
+  console.log(palindrome.reverse().join(""));
+}
