bf79b13cd29053d8808437674651352e09150eb5
Randy Madrigal <randymadrigalx12@gmail.com>
2024-04-03 21:58:17 -0400

    palindrome checker done

--- Palindrome_checker/src/script.js
+++ Palindrome_checker/src/script.js
@@ -2,26 +2,47 @@ const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const form = document.getElementById("myForm");
 const result = document.getElementById("result");
-let palindrome = [];
+let palindrome;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  InformationDiv();
 
   switch (textInput.value.trim().length) {
     case 0:
       alert("Please input a value");
       break;
+
     case 1:
       result.innerText = `${textInput.value} is a palindrome`;
+      reset();
       break;
+
     default:
       isPalindrome(textInput.value);
+      reset();
   }
 });
 
-function validate() {}
+function reset() {
+  textInput.value = "";
+}
 
-function isPalindrome(word) {
-  palindrome = [...word.toLowerCase()];
-  console.log(palindrome.reverse().join(""));
+function InformationDiv() {
+  result.classList.remove("hide");
+}
+
+function cleanInput(arg) {
+  const regEx = /[^A-Za-z0-9]/gi;
+  return arg.replace(regEx, "").toLowerCase();
+}
+
+function isPalindrome(arg) {
+  palindrome = cleanInput(arg).split("").reverse().join("");
+
+  if (palindrome === cleanInput(arg)) {
+    result.innerText = `${textInput.value} is a palindrome`;
+  } else {
+    result.innerText = `${textInput.value} is not a palindrome`;
+  }
 }
