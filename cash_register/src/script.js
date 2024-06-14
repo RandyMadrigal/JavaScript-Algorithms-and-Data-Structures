7a7702e028956c433f63e0a7093f2cc9f0b4e7b8
Randy Madrigal <randymadrigalx12@gmail.com>
2024-06-14 18:22:33 -0400

    fix the style in the app

--- cash_register/src/script.js
+++ cash_register/src/script.js
@@ -1,6 +1,9 @@
 const infoDiv = document.getElementById("infoChange");
-
-let price = 1.87;
+const form = document.querySelector("form");
+const btnPurchase = document.getElementById("purchase-btn");
+const cash = document.getElementById("input-customer");
+const screen = document.getElementById("price");
+const price = 1.87;
 
 let cid = [
   ["PENNY", 1.01],
@@ -14,7 +17,19 @@ let cid = [
   ["ONE HUNDRED", 100],
 ];
 
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+btnPurchase.addEventListener("click", () => {
+  if (cash.value < price) {
+    alert("Customer does not have enough money to purchase the item");
+  }
+});
+
 const updateUI = (arr) => {
+  screen.textContent = `RD$ ${price}`;
+
   cid.forEach((arr) => {
     const pTag = document.createElement("p");
     pTag.appendChild(document.createTextNode(`${arr[0]} : ${arr[1]}`));
