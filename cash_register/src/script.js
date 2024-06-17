091bf2e6bbdb2e42a48a9a04b2de27a84ffff880
Randy Madrigal <randymadrigalx12@gmail.com>
2024-06-17 17:52:55 -0400

    working - changeDue Function

--- cash_register/src/script.js
+++ cash_register/src/script.js
@@ -3,6 +3,7 @@ const form = document.querySelector("form");
 const btnPurchase = document.getElementById("purchase-btn");
 const cash = document.getElementById("input-customer");
 const screen = document.getElementById("price");
+const changeDue = document.getElementById("change-due");
 const price = 1.87;
 
 let cid = [
@@ -24,11 +25,42 @@ form.addEventListener("click", (e) => {
 btnPurchase.addEventListener("click", () => {
   if (cash.value < price) {
     alert("Customer does not have enough money to purchase the item");
+    cash.value = "";
+    return;
   }
+
+  if (Number(cash.value) === price) {
+    changeDue.textContent = "No change due - customer paid with exact cash";
+    cash.value = "";
+    return;
+  }
+
+  let change = Number(cash.value) - price; //devolver al cliente
+  let reversedCid = [...cid].reverse(); // devolver con las mayores primero.
+  let totalCID = parseFloat(
+    cid
+      .map((total) => total[1])
+      .reduce((prev, curr) => prev + curr)
+      .toFixed(2)
+  ); //Total de dinero en caja.
+
+  for (let i = 0; i < cid.length; i++) {
+    if (cash.value < cid[i][1]) {
+      change = (cash.value - price).toFixed(2);
+      cid[i][1] = (cid[i][1] - change).toFixed(2);
+      break;
+    }
+  }
+
+  updateUI(cid);
 });
 
 const updateUI = (arr) => {
   screen.textContent = `RD$ ${price}`;
+
+  if (infoDiv.childElementCount === arr.length) {
+    infoDiv.innerHTML = "";
+  }
 
   cid.forEach((arr) => {
     const pTag = document.createElement("p");
